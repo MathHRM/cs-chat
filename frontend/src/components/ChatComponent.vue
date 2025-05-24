b4d5776d0ecf16f45035e55c496adc860a95105a
Matheus <matheushrm614@gmail.com>
2025-05-24 11:08:26 -0300

    add chat components and connection with hub

---
+++ frontend/src/components/ChatComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="q-pa-md row justify-center">
+    <div style="width: 100%; max-width: 400px">
+    <div style="width: 100%" v-for="(message, index) in messages" :key="index">
+      <q-chat-message
+        :name="[message.username]"
+        :text="[message.content]"
+        sent
+        :stamp="[message.created_at]"
+        v-if="message.username == userActual"
+      />
+      <q-chat-message
+        :name="[message.username]"
+        :text="[message.content]"
+        :stamp="[message.created_at]"
+      /></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChatComponent',
+  props: {
+    userActual: String,
+    messages: Array
+  }
+}
+</script>
