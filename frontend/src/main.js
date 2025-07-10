8e89fc0f193e7f9209624819c4fc867d739895fa
Matheus Res <matheushrm614@gmail.com>
2025-07-10 20:33:24 -0300

    feat: implement auth store

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,10 @@ import App from './App.vue'
 import { Quasar } from 'quasar'
 import quasarUserOptions from './quasar-user-options'
 import './styles/app.css'
+import { createPinia } from 'pinia'
 
-createApp(App).use(Quasar, quasarUserOptions).mount('#app')
+const app = createApp(App);
+
+app.use(createPinia());
+
+app.use(Quasar, quasarUserOptions).mount('#app')
---
+++ frontend/src/stores/auth.js
@@ -0,0 +1,10 @@
+import { defineStore } from 'pinia';
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({ user: {} }),
+  actions: {
+    setUser(user) {
+      this.user = user;
+    },
+  },
+});
