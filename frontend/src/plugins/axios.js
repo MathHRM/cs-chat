0ffaffc2b547972d7fb607de3bbf1e187c1697f9
Matheus Res <matheushrm614@gmail.com>
2025-07-10 21:04:09 -0300

    feat: add api url env

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
 axios.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("@auth");
