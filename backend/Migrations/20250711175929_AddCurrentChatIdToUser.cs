739e61b93167d8b8ec48add042b73aa2372027b7
Matheus Res <matheushrm614@gmail.com>
2025-07-11 15:00:20 -0300

    feat: add migrations to current chat id

---
+++ backend/Migrations/20250711175929_AddCurrentChatIdToUser.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCurrentChatIdToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CurrentChatId",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CurrentChatId",
+                table: "Users",
+                column: "CurrentChatId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Chats_CurrentChatId",
+                table: "Users",
+                column: "CurrentChatId",
+                principalTable: "Chats",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Chats_CurrentChatId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_CurrentChatId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CurrentChatId",
+                table: "Users");
+        }
+    }
+}
--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -9,6 +9,7 @@ public class User
     public string Username { get; set; }
     public string Password { get; set; }
     public string? CurrentChatId { get; set; }
+    public Chat? CurrentChat { get; set; }
     public ICollection<ChatUser> ChatUsers { get; set; } = new List<ChatUser>();
     public ICollection<Message> Messages { get; set; } = new List<Message>();
 }
