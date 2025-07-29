f167c0adb90129ce23acd935c2ea0bb4167cdd61
Matheus Res <matheushrm614@gmail.com>
2025-07-29 17:29:20 -0300

    refactor: change help command to return a list of command resources

---
+++ backend/Http/Responses/CommandResponse.cs
@@ -0,0 +1,9 @@
+namespace backend.Http.Responses;
+
+public class CommandResponse
+{
+    public string? Command { get; set; }
+    public string? Description { get; set; }
+    public List<ArgumentResponse>? Arguments { get; set; }
+    public List<ArgumentResponse>? Options { get; set; }
+}
